b400395e4a791bd49e447587bf2f158c6b5d5fe4
marcel <marcel@debian>
2015-10-16 20:22:44 +0200

    okay

--- cron.js
+++ cron.js
@@ -2,7 +2,7 @@
 
 var db = require('./modules/db');
 db.init(process.env.OPENSHIFT_POSTGRESQL_DB_URL);
-var reqs = require('./requests');
+var reqs = require('./modules/requests');
 
 db.query('INSERT INTO "Snapshots" ("year", "month", "date_of_update", "content") VALUES ($1, $2, $3, $4)', 
 	[1979, Math.floor(Math.random() * 12), new Date(), JSON.stringify(new Date())], 
