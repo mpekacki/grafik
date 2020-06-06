b38e84ec858f1631b41d42b2a7b350f5d60bf51d
mpekacki <marcel.pekacki@gmail.com>
2020-06-06 20:50:33 +0200

    add agent to request

--- modules/requests.js
+++ modules/requests.js
@@ -5,10 +5,21 @@ var log = (process.env.ENV === 'development');
 
 module.exports = {
 	makeHttpGet : function(url, cb){
+		var agentOptions;
+		var agent;
+
+		agentOptions = {
+		  host: 'www.fantastyka.pl'
+		, rejectUnauthorized: false
+		};
+
+		agent = new https.Agent(agentOptions);
+		
 		if (log) console.log('making request to ' + url);
-		request(url, function(err, response, body){
+		request(url, agent: agent, function(err, response, body){
 			if (err) {
 				cb(err);
+				return;
 			}
 			var $ = cheerio.load(body, { decodeEntities: false });
 			cb(err, $);
